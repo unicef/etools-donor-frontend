fb696eb263f7bc27b7b349241fc371c3dfbebe21
Marko <marko.barca10@gmail.com>
2019-11-15 12:14:13 -0500

    fix test

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from 'redux-starter-kit';
 import createSagaMiddleware from 'redux-saga';
-import rootReducer from 'reducers';
-import rootSaga from 'sagas';
+import rootReducer from './reducers';
+import rootSaga from './sagas';
 const sagas = createSagaMiddleware();
 
 const store = configureStore({ reducer: rootReducer, middleware: [sagas] });
