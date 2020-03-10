92e579778948a6c41410fa0750c309ae15093c0f
drzackyll <adams.z.d@gmail.com>
2020-03-10 10:21:38 -0400

    Adds logout button and link in header

--- src/components/App-Bar.jsx
+++ src/components/App-Bar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { createStyles } from '@material-ui/styles';
-import { makeStyles, AppBar, Toolbar, Box, Typography } from '@material-ui/core';
+import { makeStyles, AppBar, Toolbar, Box, Typography, IconButton } from '@material-ui/core';
 import horizontalLogo from '../assets/images/UNICEF_White_Horizontal.png';
 import ErrorsSnackbar from './Errors-Snackbar';
 import { selectUserName } from 'selectors/ui-flags';
 import SuccessSnackbar from './Success-Snackbar';
 import clsx from 'clsx';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
 const useStyles = makeStyles(theme =>
   createStyles({
@@ -42,6 +43,9 @@ const useStyles = makeStyles(theme =>
     },
     appTitle: {
       paddingLeft: '12px'
+    },
+    button: {
+      color: 'white'
     }
   })
 );
@@ -49,12 +53,21 @@ const useStyles = makeStyles(theme =>
 export default function AppToolbar() {
   const classes = useStyles();
   const userName = useSelector(selectUserName);
+
+  function logout() {
+    const url = window.location.origin;
+    window.location.href = `${url}/social/unicef-logout`;
+  }
+
   return (
     <AppBar className={classes.appBar} position="fixed">
       <Toolbar className={classes.toolbar}>
         <Box width="100%">
-          <Box width="100%" display="block">
+          <Box width="100%" display="flex" justifyContent="space-between">
             <img src={horizontalLogo} className={classes.headerLogo} />
+            <IconButton aria-label="logout" className={classes.button} onClick={logout}>
+              <ExitToAppIcon />
+            </IconButton>
           </Box>
           <Box width="100%" display="flex" justifyContent="space-between">
             <Typography className={clsx(classes.appTitle, classes.banner)}>Donor Reporting Portal</Typography>
