ca1c5c794f150b80b3727464bf53c84c6f471d86
Domenico <dom.dinicola@gmail.com>
2020-04-04 11:56:06 -0600

    env var

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
+const tenant = process.env.REACT_APP_TENANT_NAME || 'test';
 
 app.use('/', (req, res, next) => {
   express.static(path.join(__dirname, 'build'))(req, res, next);
@@ -11,4 +12,6 @@ app.use(function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port, () => console.log(`Etools manage listening on port ${port}!`));
+app.listen(port, () => console.log(`Donor Reporting Portal start on ${port}!`));
+app.listen(tenant, () => console.log(`Tenant Name: ${tenant}`));
+
