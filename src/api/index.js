5b325e5fb92e7f481bfa61d5bb6fc0afe568637e
Domenico <dom.dinicola@gmail.com>
2020-04-03 19:49:42 -0600

    golden fix

--- src/api/index.js
+++ src/api/index.js
@@ -24,8 +24,6 @@ const getBaseOptions = () => ({
   }
 });
 
-const tenantName = () => process.env.TENANT_NAME || 'unitst';
-
 export async function get(uri, params = {}, options = getBaseOptions()) {
   const opt = {
     method: 'GET',
@@ -131,16 +129,16 @@ export function getOffices() {
 }
 
 export async function getReports(params, year = currentDate()) {
-  const computedUrl = process.env.REACT_APP_REPORTS_ENDPOINT.replace('<envvar>', tenantName()).replace('<folder>', `${year} Certified Reports`);
+  const computedUrl = process.env.REACT_APP_REPORTS_ENDPOINT.replace('<envvar>', process.env.REACT_APP_TENANT_NAME).replace('<folder>', `${year} Certified Reports`);
   return get(computedUrl, params);
 }
 
 export async function getUsGovReports(params, year) {
-  const computedUrl = process.env.REACT_APP_REPORTS_ENDPOINT.replace('<envvar>', tenantName()).replace('<folder>', `${year} US Gov Certified Reports`);
+  const computedUrl = process.env.REACT_APP_REPORTS_ENDPOINT.replace('<envvar>', process.env.REACT_APP_TENANT_NAME).replace('<folder>', `${year} US Gov Certified Reports`);
   return get(computedUrl, params);
 }
 
 export async function getThematicReports(params) {
-  const computedUrl = process.env.REACT_APP_REPORTS_ENDPOINT.replace('<envvar>', tenantName()).replace('<folder>', `Thematic%20Reports`);
+  const computedUrl = process.env.REACT_APP_REPORTS_ENDPOINT.replace('<envvar>', process.env.REACT_APP_TENANT_NAME).replace('<folder>', `Thematic%20Reports`);
   return get(computedUrl, params);
 }
