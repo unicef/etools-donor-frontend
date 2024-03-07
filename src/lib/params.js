bd0767e77b5e55ccf81c68dafe07030c82a8407a
Dan <dan.cioara@nordlogic.com>
2024-03-07 17:44:19 +0200

    adjustments

--- src/lib/params.js
+++ src/lib/params.js
@@ -15,7 +15,7 @@ export const PAGE_TITLES_MAP = {
   // [REPORTS_PATH]: 'Reports',
   [THEMATIC_GRANTS_PATH]: 'Thematic Grants',
   [GAVI_REPORTS_PATH]: 'Gavi Reports',
-  [GAVI_REPORTS_CTN_PATH]: 'Gavi adjusting CTNs',
+  [GAVI_REPORTS_CTN_PATH]: 'Gavi Adjusting CTNs',
   [GAVI_STATEMENTS_ACC_PATH]: 'Gavi Statements of Account',
   [POOLED_GRANTS_PATH]: 'Pooled Grants',
   [SEARCH_REPORTS_PATH]: 'Reports'
